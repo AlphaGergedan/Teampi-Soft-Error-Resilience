561e8f6ac15f206069e2d20588a68b91e5060874
Ben Hazelwood <benjamin.hazelwood@durham.ac.uk>
2018-01-24 15:32:07 +0000

    Remove debugging message

--- NonBlockingTest.cpp
+++ NonBlockingTest.cpp
@@ -25,9 +25,7 @@ int main(int argc, char* argv[]) {
 	if (rank == 0) {
 		msg = -1;
 		MPI_Request* request = new MPI_Request();
-    printf("Before in client: %p\n", (void *) request);
 		MPI_Isend(&msg, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, request);
-    printf("After in client: %p\n", (void *) request);
 		MPI_Wait(request, MPI_STATUS_IGNORE);
 	}
 	else if (rank == 1) {

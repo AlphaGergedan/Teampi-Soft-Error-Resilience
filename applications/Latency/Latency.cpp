a89bf03882d8425cd74d3bc0b2ccc94be92eac33
Ben Hazelwood <benjamin.hazelwood@durham.ac.uk>
2018-06-05 17:28:08 +0100

    Benchmark ready Latency

--- applications/Latency/Latency.cpp
+++ applications/Latency/Latency.cpp
@@ -7,16 +7,38 @@
 
 #include <mpi.h>
 #include <iostream>
+#include <assert.h>
+#include <cstdlib>
 
-const int NUM_TRIALS = 1e4;
+
+const int NUM_TRIALS = 100;
 const int NUM_PONGS  = 1e4;
 
+int START = 0;
+int FINISH = 0;
+int INCR = 0;
+
+int NUM_POINTS = 0;
+
+const int MAX_POINTS = 1e4;
 
 int main(int argc, char* argv[]) {
 
   MPI_Init(&argc, &argv);
 
-  MPI_Barrier(MPI_COMM_WORLD);
+  if (argc != 4) {
+    std::cout << "Usage: mpirun Latency <START> <FINISH> <INCREMENT>\n";
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  START = std::stoi(argv[1]);
+  FINISH = std::stoi(argv[2]);
+  INCR = std::stoi(argv[3]);
+
+  NUM_POINTS = (FINISH - START) / INCR;
+
+
+  assert(NUM_POINTS < MAX_POINTS);
 
   int rank;
   int size;
@@ -27,42 +49,60 @@ int main(int argc, char* argv[]) {
   const int source = 0;
   const int dest   = size - 1;
 
-  double m = 1.0/3.0;
+  char m[FINISH];
+
+  if (rank == source) {
+    for (int i=0; i < FINISH; i++) {
+      m[i] = '0';
+    }
+  }
 
   double t1 = 0.0;
   double t2 = 0.0;
 
-  double time[NUM_TRIALS];
+  double bandwidth[NUM_POINTS];
 
   MPI_Status status;
-
+  int counter;
   for (int t = 0; t < NUM_TRIALS; t++) {
     MPI_Barrier(MPI_COMM_WORLD);
-    if (rank == source) {
-      MPI_Recv(&m, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD, &status);
-      t1 = MPI_Wtime();
+    counter = 0;
+    for (int n = START; n <= FINISH; n += INCR) {
+      if (rank == source) {
+        // Handshake
+        MPI_Recv(&m, 1, MPI_CHAR, dest, 0, MPI_COMM_WORLD, &status);
+        t1 = MPI_Wtime();
 
-      for (int p = 0; p < NUM_PONGS; p++) {
-        MPI_Send(&m, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
-        MPI_Recv(&m, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD,  &status);
+        for (int p = 0; p < NUM_PONGS; p++) {
+          MPI_Send(&m, n, MPI_CHAR, dest, 0, MPI_COMM_WORLD);
+          MPI_Recv(&m, n, MPI_CHAR, dest, 1, MPI_COMM_WORLD,  &status);
+        }
+
+        t2 = MPI_Wtime();
+        double b = (sizeof(char) * n * 2.0 * (double)NUM_PONGS) / (t2 - t1);
+        if (bandwidth[counter] < b) {
+          bandwidth[counter] = b;
+        }
       }
 
-      t2 = MPI_Wtime();
-      time[t] = t2 - t1;
-    }
-
-    if (rank == dest) {
-      MPI_Send(&m, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD);
-      for (int p = 0; p < NUM_PONGS; p++) {
-        MPI_Recv(&m, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD,  &status);
-        MPI_Send(&m, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD);
+      if (rank == dest) {
+        // Handshake
+        MPI_Send(&m, 1, MPI_CHAR, source, 0, MPI_COMM_WORLD);
+        for (int p = 0; p < NUM_PONGS; p++) {
+          MPI_Recv(&m, n, MPI_CHAR, source, 0, MPI_COMM_WORLD,  &status);
+          MPI_Send(&m, n, MPI_CHAR, source, 1, MPI_COMM_WORLD);
+        }
       }
+      counter++;
     }
   }
 
+
   if (rank == source) {
-    for (int t=0; t < NUM_TRIALS; t++) {
-      std::cout << "T(" << t << ")=" << time[t] << "\n";
+    counter = 0;
+    for (int n=START; n < FINISH; n+=INCR) {
+      std::cout << "bandwidth[" << n << "]=" << bandwidth[counter] << "\n";
+      counter++;
     }
   }
 

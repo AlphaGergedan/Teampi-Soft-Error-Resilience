55de6c80f1c5c77eb9d746f9a8cabbcac72c61e8
Ben Hazelwood <benjamin.hazelwood@durham.ac.uk>
2018-07-20 15:39:58 +0100

    Tidy up PerfSimulator

--- applications/PerfSimulator/PerfSimulator.cpp
+++ applications/PerfSimulator/PerfSimulator.cpp
@@ -16,49 +16,23 @@ const int NUM_COMPUTATIONS = 5e7;
 
 int main(int argc, char *argv[])
 {
-
   MPI_Init(&argc, &argv);
-
-  // if (argc != 3)
-  // {
-  //   std::cout << "Usage: mpirun Latency <NUM TRIALS> <NUM COMPUTATIONS>\n";
-  //   MPI_Abort(MPI_COMM_WORLD, 1);
-  // }
-
-  int rank;
-  int size;
-
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  const int source = 0;
-  const int dest = size - 1;
-
-  char m = '0';
 
   for (int t = 0; t < NUM_TRIALS; t++)
   {
     MPI_Barrier(MPI_COMM_WORLD);
 #ifdef COMPARE_PROGRESS
-    MPI_Sendrecv(MPI_IN_PLACE, 0, MPI_BYTE, MPI_PROC_NULL, 0, MPI_IN_PLACE, 0, MPI_BYTE, MPI_PROC_NULL, 0, MPI_COMM_SELF, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(MPI_IN_PLACE, 0, MPI_BYTE, MPI_PROC_NULL, -1, MPI_IN_PLACE, 0, MPI_BYTE, MPI_PROC_NULL, 0, MPI_COMM_SELF, MPI_STATUS_IGNORE);
 #endif
 
     for (int i = 0; i < NUM_COMPUTATIONS; i++) {
-      sin(1.0/3.0);
+      sin(1.0/3.0); // Arbitrary computation (the compiler shouldn't optimise it out...)
     }
 
 #ifdef COMPARE_PROGRESS
     MPI_Sendrecv(MPI_IN_PLACE, 0, MPI_BYTE, MPI_PROC_NULL, 1, MPI_IN_PLACE, 0, MPI_BYTE, MPI_PROC_NULL, 0, MPI_COMM_SELF, MPI_STATUS_IGNORE);
 #endif
     MPI_Barrier(MPI_COMM_WORLD);
-    // if (rank == source)
-    // {
-    //   MPI_Recv(&m, 1, MPI_CHAR, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    // }
-    // else
-    // {
-    //   MPI_Send(&m, 1, MPI_CHAR, dest, 0, MPI_COMM_WORLD);
-    // }
   }
 
   MPI_Finalize();
